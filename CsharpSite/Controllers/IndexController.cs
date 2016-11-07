043c12003023a0513f91fb486a824e7a889abe15
Jeremi-payday <201422595@collegeahuntsic.qc.ca>
2016-11-07 12:55:47 -0500

    init project

---
+++ CsharpSite/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CsharpSite.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: indexDefault
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CsharpSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CsharpSite.Startup))]
+namespace CsharpSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
